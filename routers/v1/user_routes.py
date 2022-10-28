ab88723524061147b34819d02b52d4cea39bc8ce
lamarcke <cassiolamarcksilvafreitas@gmail.com>
2022-10-28 00:11:04 -0300

    merge

--- routers/v1/user_routes.py
+++ routers/v1/user_routes.py
@@ -80,7 +80,14 @@ async def user_recover(email=Form(...)):
 
     message = MessageSchema(subject="Recuperação de senha - Bibliomar", recipients=[user["email"]], html=html)
     fm = FastMail(conf)
-    await fm.send_message(message)
+    print("working as expected")
+    try:
+
+        await fm.send_message(message)
+    except BaseException as e:
+        print(f"error for email: {email_url}")
+        print(e)
+        pass
 
     return 200
 
