6bd2914d1af284ada5de05a6c9da2640eac0f306
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2023-01-03 01:30:55 -0300

    - Possible fix for downloads

--- services/search/download_functions.py
+++ services/search/download_functions.py
@@ -3,6 +3,7 @@ from grab_fork_from_libgen.search_config import get_request_headers
 from requests import exceptions
 from requests_html import AsyncHTMLSession
 from services.search.metadata_functions import get_dlinks
+import os
 
 import aiofiles
 from aiofiles import os as aioos
@@ -15,12 +16,16 @@ async def remove_temp_download(filename: str):
 
 
 async def _create_temp_file(content: bytes, md5: str):
-    filename = f"{temp_download_folder}/{md5}.epub"
+    filename = os.path.sep.join(temp_download_folder, f"{md5}.epub")
     async with aiofiles.open(filename, "wb+") as fo:
         await fo.write(content)
         await fo.close()
 
     return filename
+
+def _create_temp_dir():
+    if not os.path.exists(temp_download_folder):
+        os.mkdir(temp_download_folder)
 
 
 async def _download_handler(d_link: str):
@@ -31,6 +36,8 @@ async def _download_handler(d_link: str):
 
 async def make_temp_download(md5: str, topic: str):
     # We make use of a possible d_links cache by using the same function we use in /metadata.
+    # TODO: Refactor and put this somewhere else.
+    _create_temp_dir()
     d_links_handler = await get_dlinks(md5, topic)
     d_links = d_links_handler[0]
     downloaded_file = None
@@ -40,7 +47,6 @@ async def make_temp_download(md5: str, topic: str):
         try:
             tried_download = await _download_handler(link)
             temp_file = await _create_temp_file(tried_download, md5)
-            print(f"from {link}")
             if temp_file is not None:
                 downloaded_file = temp_file
                 break
@@ -48,7 +54,6 @@ async def make_temp_download(md5: str, topic: str):
                 continue
 
         except (exceptions.Timeout, exceptions.ConnectionError, exceptions.HTTPError) as err:
-            print(err)
             last_err = err
             continue
 
